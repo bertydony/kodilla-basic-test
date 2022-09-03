770867d6bb9af81a39c1034927df86cb3bb79a0f
Bartossz Dolnik <bartosz.dolnik@gmail.com>
2022-09-03 08:48:33 +0200

    Zadanie kolekcja znaczkow Poprawiobne

--- kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/Stamp.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/Stamp.java
@@ -5,12 +5,12 @@ import java.util.Objects;
 public class Stamp {
     private final String name;
     private final String size;
-    private final boolean isPunched;
+    private final boolean isStamped;
 
-    public Stamp(String name, String size, boolean isPunched) {
+    public Stamp(String name, String size, boolean isStamped) {
         this.name = name;
         this.size = size;
-        this.isPunched = isPunched;
+        this.isStamped = isStamped;
     }
 
     public String getSize() {
@@ -18,7 +18,7 @@ public class Stamp {
     }
 
     public boolean isPunched() {
-        return isPunched;
+        return isStamped;
     }
 
     public String getName() {
@@ -30,14 +30,14 @@ public class Stamp {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Stamp stamp = (Stamp) o;
-        return isPunched == stamp.isPunched
+        return isStamped == stamp.isStamped
                 && Objects.equals(name, stamp.name)
                 && Objects.equals(size, stamp.size);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, size, isPunched);
+        return Objects.hash(name, size, isStamped);
     }
 
     @Override
@@ -45,7 +45,7 @@ public class Stamp {
         return "Stamp{" +
                 "name='" + name + '\'' +
                 ", size='" + size + '\'' +
-                ", isPunched=" + isPunched +
+                ", isPunched=" + isStamped +
                 '}';
     }
 }
